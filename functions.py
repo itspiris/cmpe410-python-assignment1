9cacb04d330f98ce9c4b32e492f2523384b5fa89
itspiris <101747892+itspiris@users.noreply.github.com>
2022-03-21 23:04:40 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import student_class
 students = []
 
 def ToDo(*args):
-    print("args[0]: ",args[0])
+    #print("args[0]: ",args[0])
     if args[0] == 1:
         AddStd(args[1],args[2],args[3],args[4],args[5])
     elif args[0] == 2:
@@ -15,38 +15,68 @@ def ToDo(*args):
         ModifyStd(args[1],args[2])
     elif args[0] == 6:
         DelStd(args[1],args[2])
-    
 
 def AddStd(first_name,last_name,age,sex,major):
-    print("\nAddStd function\n")
+    #print("\nAddStd function\n")
     students.append(student_class.Student(first_name,last_name,age,sex,major))
 
 def FindStd(name,surname):
-    print("\nFindStd function\n")
+    #print("\nFindStd function\n")
     for obj in students:
         if obj.fname == name and obj.lname == surname:
+            print("\n-----------------------------------------\n")
             print("Name: "+obj.fname)
             print("Surname: "+obj.lname)
             print("Age: ",obj.age)
             print("Sex: "+obj.sex)
             print("Major: "+obj.major)
+            print("\n-----------------------------------------")
+        else:
+            print("\nNo such student exists on our system")
 
 def DispStd():
-    print("\nDispStd function\n")
+    #print("\nDispStd function\n")
+    print("\n-----------------------------------------\n")
     for obj in students:
         print("Name: "+obj.fname)
         print("Surname: "+obj.lname)
         print("Age: ",obj.age)
         print("Sex: "+obj.sex)
         print("Major: "+obj.major)
-        print()
-    print("-----------------------------------------")
+        #print()
+    print("\n-----------------------------------------")
 
-def DispFewStd():
-    print("\nDispFewStd function\n")
+def DispFewStd(min_age,max_age):
+    #print("\nDispFewStd function\n")
+    for obj in students:
+        if obj.age >= min_age and obj.age <= max_age:
+            print("Name: "+obj.fname)
+            print("Surname: "+obj.lname)
+            print("Age: ",obj.age)
+            print("Sex: "+obj.sex)
+            print("Major: "+obj.major)
+            #print()
+    print("\n-----------------------------------------")
 
-def ModifyStd():
-    print("\nModifyStd function\n")
 
-def DelStd():
-    print("\nDelStd function\n")
+def ModifyStd(fname,lname):
+    #print("\nModifyStd function\n")
+    found = False
+    for obj in students:
+        if obj.fname == fname and obj.lname == lname:
+            attbt = input("Attribute to change: ")
+            newvalue = input("New value: ")
+            obj.setAttb(attbt,newvalue)
+            found = True
+    if not found:
+        print("\nNo such student exists on our system")
+
+def DelStd(fname,lname):
+    #print("\nDelStd function\n")
+    found = False
+    for obj in students:
+        if obj.fname == fname and obj.lname == lname:
+            students.remove(obj)
+            found = True
+    if not found:
+        print("\nNo such student exists on our system")
