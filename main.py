e2ad2bbbd1409efad900506a3b6943079e3195fc
itspiris <101747892+itspiris@users.noreply.github.com>
2022-03-21 23:05:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import functions
 s1 = student_class.Student("piris","the assistant",20,"male","computer engineer")
 functions.students.append(s1)
 print()
-print("I am the student council, " + s1.getAttb("first name"))
-print(functions.students)
 
-choice = int(input("Enter the integer corresponding to what you need: "))
+def options():
+    print("Enter the integer corresponding to what you need: ")
+    print("\t1 : Add a student\n\t2 : Find a specific student\n\t3 : Display all students stored")
+    print("\t4 : Display students in certain age range\n\t5 : Modify an attribute of a specific student")
+    print("\t6 : Delete a student with a specific first name-last name\n\t7 : exit")
+options()
+choice = int(input("\nAction: "))
 print()
 while choice != 7:
     if choice == 1:
         fname = input("Enter first name of student: ")
         lname = input("Enter last name of student: ")
-        age = input("Enter age of student: ")
+        age = int(input("Enter age of student: "))
         sex = input("Enter gender of student: ")
         major = input("Enter the major of the student: ")
         functions.ToDo(choice,fname,lname,age,sex,major)
@@ -23,8 +27,22 @@ while choice != 7:
         functions.ToDo(choice,fname,lname)
     elif choice == 3:
         functions.ToDo(choice)
+    elif choice == 4:
+        min_age = int(input("Minimum age: "))
+        max_age = int(input("Maximum age: "))
+        functions.ToDo(choice,min_age,max_age)
+    elif choice == 5:
+        name = input("Name: ")
+        surname = input("Surname: ")
+        functions.ToDo(choice,name,surname)
+    elif choice == 6:
+        name = input("Name of student to be deleted: ")
+        surname = input("Surname of student to be deleted: ")
+        functions.ToDo(choice,name,surname)
     
     print()
-    choice = int(input("Enter the integer corresponding to what you need: "))
+    options()
+    choice = int(input("\nAction : "))
+    print()
 
 print()
