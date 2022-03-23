491426748767b3715383dbadcfc3751c2eeb046d
itspiris <101747892+itspiris@users.noreply.github.com>
2022-03-23 23:14:25 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import student_class
-students = []
 
+students = []
 def ToDo(*args):
     #print("args[0]: ",args[0])
     if args[0] == 1:
@@ -15,6 +15,20 @@ def ToDo(*args):
         ModifyStd(args[1],args[2])
     elif args[0] == 6:
         DelStd(args[1],args[2])
+    elif args[0] == 7:
+        args[1].write_data_to_file()
+    elif args[0] == 8:
+        args[1].read_data_from_a_file()
+    
+    """options = {1 : AddStd(args[1],args[2],args[3],args[4],args[5]),
+               2 : FindStd(args[1],args[2]),
+               3 : DispStd,
+               4 : DispFewStd,
+               5 : ModifyStd,
+               6 : DelStd,
+               7 : exit}
+    print(type(options))
+    options[args[0]]"""
 
 def AddStd(first_name,last_name,age,sex,major):
     #print("\nAddStd function\n")
@@ -22,8 +36,10 @@ def AddStd(first_name,last_name,age,sex,major):
 
 def FindStd(name,surname):
     #print("\nFindStd function\n")
+    found = False
     for obj in students:
         if obj.fname == name and obj.lname == surname:
+            found = True
             print("\n-----------------------------------------\n")
             print("Name: "+obj.fname)
             print("Surname: "+obj.lname)
@@ -31,8 +47,8 @@ def FindStd(name,surname):
             print("Sex: "+obj.sex)
             print("Major: "+obj.major)
             print("\n-----------------------------------------")
-        else:
-            print("\nNo such student exists on our system")
+    if not found:
+        print("\nNo such student exists on our system")
 
 def DispStd():
     #print("\nDispStd function\n")
@@ -43,13 +59,14 @@ def DispStd():
         print("Age: ",obj.age)
         print("Sex: "+obj.sex)
         print("Major: "+obj.major)
-        #print()
-    print("\n-----------------------------------------")
+        print()
+    print("-----------------------------------------")
 
 def DispFewStd(min_age,max_age):
     #print("\nDispFewStd function\n")
     for obj in students:
         if obj.age >= min_age and obj.age <= max_age:
+            print("\n-----------------------------------------\n")
             print("Name: "+obj.fname)
             print("Surname: "+obj.lname)
             print("Age: ",obj.age)
